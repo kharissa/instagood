5292da0b33cb62eb8486b419fb61205023bdd2fe
kharissa <kharissafernando@gmail.com>
2019-03-28 18:11:40 +0800

    Update validation for unique email and username. Create HTML form.

--- models/base_model.py
+++ models/base_model.py
@@ -2,27 +2,27 @@ import os
 import peewee as pw
 import datetime
 from database import db
-
+from peewee_validates import ModelValidator
 
 class BaseModel(pw.Model):
     created_at = pw.DateTimeField(default=datetime.datetime.now)
     updated_at = pw.DateTimeField(default=datetime.datetime.now)
 
     def save(self, *args, **kwargs):
-        self.errors = []
-        self.validate()
+        validator = type(self).CustomValidator(self)
+        validator.validate()
 
-        if len(self.errors) == 0:
+        self.errors = validator.errors
+
+        if self.errors:
+            return 0
+        else:
             self.updated_at = datetime.datetime.now()
             return super(BaseModel, self).save(*args, **kwargs)
-        else:
-            return 0
-
-    def validate(self):
-        print(
-            f"Warning validation method not implemented for {str(type(self))}")
-        return True
 
     class Meta:
         database = db
         legacy_table_names = False
+
+    class CustomValidator(ModelValidator):
+        pass
--- models/user.py
+++ models/user.py
@@ -1,6 +1,44 @@
 from models.base_model import BaseModel
 import peewee as pw
-
+from peewee_validates import ModelValidator, StringField, validate_email
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    name = pw.CharField()
+    email = pw.CharField(unique=True)
+    username = pw.CharField(unique=True)
+    password = pw.CharField()
+
+    def save(self, *args, **kwargs):
+        # Ensure all fields are entered and email is valid
+        validator = type(self).CustomValidator(self)
+        validator.validate()
+        self.errors = validator.errors
+
+        # Ensure unique email and username
+        validator = ModelValidator(self)
+        validator.validate()
+        self.errors.update(validator.errors)
+        
+        if self.errors:
+            print(self.errors)
+            return 0
+        else:
+            self.updated_at = datetime.datetime.now()
+            return super(BaseModel, self).save(*args, **kwargs)
+
+
+    class CustomValidator(ModelValidator):
+        name = StringField(required=True)
+        email = StringField(required=True, validators=[validate_email()])
+        username = StringField(required=True)
+        password = StringField(required=True)
+
+    class Meta:
+        messages = {
+            'name.required': 'Enter your name.',
+            'email.required': 'Enter your email address.',
+            'username.required': 'Create a username.',
+            'password.required': 'Create a password.',
+            'email.validators': 'Enter a correct email address.'
+        }
+
